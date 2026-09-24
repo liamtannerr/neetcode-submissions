6086f9b5f7b0cb6f6bdd77a7c8bc094cb602134c
Liam Tanner <128447390+liamtannerr@users.noreply.github.com>
2026-09-24 16:47:09 -0700

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,23 @@
+# Pattern should go 1, 2, 3, 5, 8, 13, 21 .....
+
+# n = 5
+# My algorithm: res = 5, prev = 3, prevX2 = 3, temp = 3
+
+
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n <= 2:
+            return n
+
+        p1 = 2
+        p2 = 1
+        res = p1 + p2
+        for i in range(3,n):
+            p2 = p1
+            p1 = res
+            res = p1 + p2
+
+        return res
